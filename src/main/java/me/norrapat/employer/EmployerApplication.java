cb8deaedc0c3fb4541458b19b63cd96cc8458677
Norrapat Nimmanee <charge_n@hotmail.com>
2021-03-12 21:13:50 +0700

    Initial Project

---
+++ src/main/java/me/norrapat/employer/EmployerApplication.java
@@ -0,0 +1,13 @@
+package me.norrapat.employer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/norrapat/employer/EmployerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.norrapat.employer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
