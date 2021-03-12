bddb7f4f2befac9bf7bdc9d6fabadbe9d732fd00
Norrapat Nimmanee <charge_n@hotmail.com>
2021-03-12 22:52:56 +0700

    add config oauth

--- src/main/java/me/norrapat/employer/EmployerApplication.java
+++ src/main/java/me/norrapat/employer/EmployerApplication.java
@@ -2,12 +2,14 @@ package me.norrapat.employer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
+@EnableResourceServer
 @SpringBootApplication
 public class EmployerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmployerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EmployerApplication.class, args);
+    }
 
 }
---
+++ src/main/java/me/norrapat/employer/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package me.norrapat.employer.config;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@EnableAutoConfiguration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/oauth/**").permitAll()
+                .and()
+                .csrf().disable()
+        ;
+    }
+}
