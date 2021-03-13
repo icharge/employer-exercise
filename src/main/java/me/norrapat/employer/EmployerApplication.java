19a01166a2f4cb2165a74b1127c3b2c1cc63da99
Norrapat Nimmanee <charge_n@hotmail.com>
2021-03-13 12:41:15 +0700

    setup oauth

--- src/main/java/me/norrapat/employer/EmployerApplication.java
+++ src/main/java/me/norrapat/employer/EmployerApplication.java
@@ -2,9 +2,7 @@ package me.norrapat.employer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
-@EnableResourceServer
 @SpringBootApplication
 public class EmployerApplication {
 
--- src/main/java/me/norrapat/employer/config/SecurityConfig.java
+++ src/main/java/me/norrapat/employer/config/SecurityConfig.java
@@ -1,21 +1,39 @@
 package me.norrapat.employer.config;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@EnableWebSecurity
-@EnableAutoConfiguration
+@Configuration
+@Order(1)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/oauth/**").permitAll()
+        http.requestMatchers()
+                .antMatchers("/login", "/auth/authorize")
                 .and()
-                .csrf().disable()
-        ;
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("humptydumpty")
+                .password(passwordEncoder().encode("123456"))
+                .roles("USER");
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
