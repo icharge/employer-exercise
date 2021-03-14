94e53e22d5666e2f9ef46c3ae49d3d26386363f4
Norrapat Nimmanee <charge_n@hotmail.com>
2021-03-15 03:42:29 +0700

    add Rest testing should return hello

---
+++ src/test/java/me/norrapat/employer/controller/HelloHttpTest.java
@@ -0,0 +1,29 @@
+package me.norrapat.employer.controller;
+
+import me.norrapat.employer.dto.GenericResponse;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HelloHttpTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void helloShouldReturnHelloMessage() {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/", GenericResponse.class))
+                .extracting("message")
+                .hasNoNullFieldsOrProperties()
+                .isEqualTo("Hello");
+    }
+
+}
